dbc7fbeb01e6e0d3afa17fa84f8dfa1f5b66cd55
Daniel Vlasceanu <alexvlasceanoo@gmail.com>
2026-03-06 12:37:34 +0200

    Add Frames and iFrames tests

---
+++ src/test/java/helperMethods/FrameMethods.java
@@ -0,0 +1,24 @@
+package helperMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FrameMethods {
+    public WebDriver driver;
+
+    public FrameMethods(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void switchToFrame(String text) {
+        driver.switchTo().frame(text);
+    }
+
+    public void switchToIFrame(WebElement element) {
+        driver.switchTo().frame(element);
+    }
+
+    public void defaultContent() {
+        driver.switchTo().defaultContent();
+    }
+}
